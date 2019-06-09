5968da42434138a3438ddfaccd356639e72da1c0
RedWorz <48109522+RedWorz@users.noreply.github.com>
2019-06-10 01:20:02 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -158,18 +158,5 @@ client.on('message', function (message) {
  
 })
 
-client.on('guildMemberAdd', function (member) {
-    let embed = new Discord.RichEmbed()
-        .setDescription(':tada: **' + member.user.username + '** a rejoint ' + member.guild.name)
-        .setFooter('Nous sommes désormais ' + member.guild.memberCount)
-    member.guild.channels.get('587406139042562049').send(embed)
-    member.addRole('587345387912953867')
-})
-client.on('guildMemberRemove', function (member) {
-    let embed = new Discord.RichEmbed()
-        .setDescription(':cry: **' + member.user.username + '** a quitté ' + member.guild.name)
-        .setFooter('Nous sommes désormais ' + member.guild.memberCount)
-    member.guild.channels.get('587406139042562049').send(embed)
-})
 
 
