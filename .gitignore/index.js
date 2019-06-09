5b98258453b30f4df2f33b4bf86d98694edcaf4f
RedWorz <48109522+RedWorz@users.noreply.github.com>
2019-06-09 14:37:38 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -157,28 +157,3 @@ client.on('message', function (message) {
     }
 })
 
-// Musique
-
-const ytdl = require('ytdl-core');
-
-module.exports.run = async (bot, message, args) => {
-    if (!message.member.voiceChannel) 
-        return message.channel.send('Connectez vous a un salon vocal pour éxecuter cette commande :x');
-    if (message.guild.me.voiceChannel) 
-        return message.channel.send('Le bot et dèja connecter a un autres salon !');
-    if (!args[0]) 
-        return message.channel.send('Merci de mettre un lien youtube après la commande');
-
-    const validate = await ytdl.validateURL(args[0]);
-    if (!validate) return message.channel.send ("Votre url n'est pas valide :x")
-
-    const info = await ytdl.getInfo(args[0]);
-    const connection = await message.member.voiceChannel.join();
-    const dispatcher = await connection.playStream(ytdl(args[0], { filter: 'audioonly'})
-    );
-    message.channel.send(`Musique ajoutée : ${info.title}`)
-};
-
-module.exports.help = {
-    name:'play'
-};
